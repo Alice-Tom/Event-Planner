d38100e85a9f65c6a350c6a6ac8ba18ff598c60b
Alice-Tom <admachuve@gmail.com>
2020-09-25 13:24:21 +0300

    initial commit

---
+++ register.php
@@ -0,0 +1,91 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>The Event Planner - Register</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Event Login Form template Responsive, Login form web template,Flat Pricing tables,Flat Drop downs  Sign up Web Templates, Flat Web Templates, Login sign up Responsive web template, SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style3.css" rel="stylesheet" type="text/css" media="all" />
+<!-- //Custom Theme files -->
+<!-- web font -->  
+<link href="//fonts.googleapis.com/css?family=Ubuntu:300,300i,400,400i,500,500i,700,700i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext" rel="stylesheet">
+<!-- //web font --> 
+</head>
+<body>
+	<!-- main -->
+	<div class="main">
+    
+              <div class="col-lg-12">
+						<div class="title-box">
+							<h2><span>The Event Planner</span></h2>
+						</div>
+				</div>
+						</div>
+		<div class="main-w3lsrow">
+			<!-- login form -->
+			<div class="login-form login-form-left"> 
+				<div class="agile-row">
+					  
+                 <div class="col-lg-12">
+						<div class="title-box">
+							<h2>Register<span></span></h2>
+					</div>
+					</div>
+					<div class="login-agileits-top"> 	
+						<form action="#" method="post"> 
+                        
+                    </div>
+					
+					<div class="col-lg-12">
+						<div class="title-box">
+							<p>Username<span></span></p>
+					    </div>
+					</div>
+					<input type="text" class="password" name="username" required=""/>  
+
+
+					<div class="col-lg-12">
+						<div class="title-box">
+							<p>Password<span></span></p>
+					    </div>
+					</div>
+						<input type="password" class="password" name="password" required=""/>  
+
+
+                    <div class="col-lg-12">
+						<div class="title-box">
+							<p>Email<span></span></p>
+					    </div>
+					</div>
+					<input type="text" class="password" name="email" required=""/>  
+
+
+					<div class="col-lg-12">
+						<div class="title-box">
+							<p>Phone Number<span></span></p>
+					    </div>
+					</div>
+					<input type="text" class="password" name="pnumber" required=""/>  
+
+							  
+							<input type="submit" value="Register"> 
+						</form> 	
+					</div> 
+					
+				</div>  
+			</div>  
+		</div>	
+		
+	</div>	
+	<!-- //main --> 
+</body>
+</html>
