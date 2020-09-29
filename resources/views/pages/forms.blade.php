909ad246155e4b463104670d84fdd43e6cb1e73d
Prince <stephenprince19@gmail.com>
2020-09-29 09:44:18 +0300

    Registration

--- resources/views/pages/forms.blade.php
+++ resources/views/pages/forms.blade.php
@@ -5,6 +5,9 @@
 		<title>Flavour Events</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+		<!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
 		<!-- STYLE CSS -->
 		<link rel="stylesheet" href="css/events.css">
 		
